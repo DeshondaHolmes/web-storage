33285298cb3a4ce9392a1b6e914a73daf168802b
Deshonda Holmes <Deshonda.holmes@gmail.com>
2023-06-15 16:14:42 -0400

    Add boilerplate bootstrap and buttons

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+
+
+"use strict";
+
+const studentOption = document.getElementById("studentOption");
+const instructorOption = document.getElementById("instructorOption");
+const nameId = document.getElementById("nameId");
+// const enterButton= document.getElementById("enterButton");
+
+const studentName = "Student";
+const instructorName = "Instructor";
+
+
+
+
+
+
+window.localStorage = init;
+
+function init() {
+
+    // localStorage.setItem("lastProduct", lastProductViewed);
+   
+
+    
+    
+}
+
+function onEnterButtonClick() {
+    
+    localStorage.setItem("Student", studentOption);
+
+}
+
+
